4a55207da7e0cba4348332388ba135da09d4b8ea
juuulialopes <arrudamlopes@gmail.com>
2024-09-07 23:03:44 +0000

    link CSS

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 function Home () {
+
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "style.css"; // Substitua pelo caminho do seu arquivo CSS
+
+// Adicionar o link no head do documento
+document.head.appendChild(link);
     return <h1>Benho, eu te amo e dê uma risadinha pq sim </h1>
 }
 
